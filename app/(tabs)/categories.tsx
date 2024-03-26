a89dc4ef3bb3c97dde6ecc7b765aacf076ffd2ac
Green Gem <chris_waitt@me.com>
2024-03-26 17:00:09 +0000

    Add color to category list items

--- app/(tabs)/categories.tsx
+++ app/(tabs)/categories.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useCallback } from 'react';
-import { ListItem, XStack, YGroup, YStack, Text } from 'tamagui';
+import { ListItem, XStack, YGroup, YStack, Text, Circle } from 'tamagui';
 import { IconX } from '@tabler/icons-react-native';
-import { addCategory, deleteCategory, fetchCategories } from '../services/database';
+import { deleteCategory, fetchCategories } from '../services/database';
 import { useFocusEffect } from 'expo-router';
 
 interface Category {
   id: string;
   name: string;
+  color: string;
 }
 
 export default function TabCategoryScreen() {
@@ -34,8 +35,11 @@ export default function TabCategoryScreen() {
         {categories.map((category) => (
           <YGroup.Item key={category.id}>
             <ListItem>
-              <XStack flex={1} justifyContent='space-between'>
-                <Text>{category.name}</Text>
+              <XStack flex={1} justifyContent='space-between' alignItems='center'>
+                <XStack alignItems='center' gap="$3">
+                  <Circle backgroundColor={category.color} size="$1" />
+                  <Text>{category.name}</Text>
+                </XStack>
                 <IconX size={20} color="red" onPress={() => handleDeleteCategory(category.name)} />
               </XStack>
             </ListItem>
