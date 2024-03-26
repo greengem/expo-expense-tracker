6db0d4c13c486017f70d4d81957840cdd6cfdd68
Green Gem <chris_waitt@me.com>
2024-03-26 16:27:20 +0000

    Delete categories.ts and modal.tsx files

--- app/categories.ts
+++
@@ -1,9 +0,0 @@
-export const categories = [
-    { name: "Food", slug: "food" },
-    { name: "Transport", slug: "transport" },
-    { name: "Entertainment", slug: "entertainment" },
-    { name: "Shopping", slug: "shopping" },
-    { name: "Health", slug: "health" },
-    { name: "Bills", slug: "bills" },
-    { name: "Other", slug: "other" },
-]
