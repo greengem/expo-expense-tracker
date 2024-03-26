bfbc12c87fc67201aa2ffdbc8a456b0146b44540
Green Gem <chris_waitt@me.com>
2024-03-26 17:00:04 +0000

    Add color selection to new category modal

--- app/new-category-modal.tsx
+++ app/new-category-modal.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import { YStack, Input, Button, XStack, Circle } from "tamagui";
 import { addCategory } from './services/database';
+import { router } from 'expo-router';
 
 export default function ModalScreen() {
   const [newCategoryName, setNewCategoryName] = useState('');
+  const [selectedColor, setSelectedColor] = useState('');
 
   const handleAddCategory = async () => {
-    if (newCategoryName.trim() === '') return;
-    await addCategory(newCategoryName);
+    if (newCategoryName.trim() === '' || selectedColor.trim() === '') return;
+    await addCategory(newCategoryName, selectedColor);
     setNewCategoryName('');
+    setSelectedColor('');
+
+    router.back();
   };
 
   const labelColours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'pink'];
@@ -21,8 +26,15 @@ export default function ModalScreen() {
         autoFocus
       />
       <XStack gap="$2">
-        {labelColours.map((color) => (
-          <Circle key={color} size="$2" backgroundColor={color} borderColor="white" borderWidth="$0.75" />
+      {labelColours.map((color) => (
+          <Circle
+            key={color}
+            size="$3"
+            backgroundColor={color}
+            borderColor={selectedColor === color ? "white" : "black"}
+            borderWidth={selectedColor === color ? "$1" : "$0"}
+            onPress={() => setSelectedColor(color)}
+          />
         ))}
       </XStack>
       <Button onPress={handleAddCategory}>Add Category</Button>
