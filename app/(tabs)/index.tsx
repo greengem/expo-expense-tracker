6069488fc6507c8d6c1c00651e7e932fedd1b5e7
Green Gem <chris_waitt@me.com>
2024-03-26 16:27:43 +0000

    Add time frame selection and navigation buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
-import { Text, Card, YStack } from 'tamagui'
+import { Text, Card, YStack, XStack, Button } from 'tamagui'
 import { useFocusEffect } from 'expo-router';
 import { useState, useCallback } from 'react';
 import { fetchCurrentBalance } from '../services/database';
+import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react-native';
 
 export default function TabOneScreen() {
   const [currentBalance, setCurrentBalance] = useState<number | null>(null);
+  const [timeFrame, setTimeFrame] = useState('month');
 
   useFocusEffect(
     useCallback(() => {
@@ -21,13 +23,35 @@ export default function TabOneScreen() {
     }, [])
   );
 
+  const handlePrev = () => {
+    // Logic to subtract one day/month/year from selectedDate
+  };
+
+  const handleNext = () => {
+    // Logic to add one day/month/year to selectedDate
+  };
+
+
+
   const formattedBalance = currentBalance !== null ? `$${currentBalance.toFixed(2)}` : '$0.00';
 
   return (
     <YStack gap="$3" padding="$3">
+      {/* <XStack justifyContent='space-between'>
+        <Button variant='outlined' size="$3" onPress={() => setTimeFrame('day')}>Day</Button>
+        <Button variant='outlined' size="$3" onPress={() => setTimeFrame('month')}>Month</Button>
+        <Button variant='outlined' size="$3" onPress={() => setTimeFrame('year')}>Year</Button>
+      </XStack>
+
+      <XStack justifyContent='space-between'>
+        <Button size="$3" onPress={handlePrev}><IconChevronLeft /></Button>
+        <Text>The Date</Text>
+        <Button size="$3" onPress={handleNext}><IconChevronRight /></Button>
+      </XStack> */}
+
       <Card>
         <Card.Header padded>
-          <Text>Current Balance</Text>
+          <Text>Expenses</Text>
         </Card.Header>
         <Card.Footer padded><Text fontSize="$10">{formattedBalance}</Text></Card.Footer>
       </Card>
