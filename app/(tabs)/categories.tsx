0e7d5fe429caf8204e11b0db3bbf6bc0882d0f12
Green Gem <chris_waitt@me.com>
2024-03-26 16:27:32 +0000

    Refactor TabCategoryScreen component to fetch and display categories from the database

--- app/(tabs)/categories.tsx
+++ app/(tabs)/categories.tsx
@@ -1,29 +1,47 @@
-import React from 'react';
-import { ListItem, XStack, YGroup, YStack, Input, Text, Button } from 'tamagui'
-import { categories } from '../categories';
+import React, { useState, useCallback } from 'react';
+import { ListItem, XStack, YGroup, YStack, Text } from 'tamagui';
 import { IconX } from '@tabler/icons-react-native';
+import { addCategory, deleteCategory, fetchCategories } from '../services/database';
+import { useFocusEffect } from 'expo-router';
+
+interface Category {
+  id: string;
+  name: string;
+}
 
 export default function TabCategoryScreen() {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const loadCategories = useCallback(async () => {
+    const fetchedCategories = await fetchCategories();
+    setCategories(fetchedCategories);
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      loadCategories();
+    }, [loadCategories])
+  );
+
+  const handleDeleteCategory = async (name: string) => {
+    await deleteCategory(name);
+    loadCategories();
+  };
 
   return (
     <YStack padding="$3" gap="$5">
       <YGroup>
         {categories.map((category) => (
-          <YGroup.Item key={category.slug}>
+          <YGroup.Item key={category.id}>
             <ListItem>
               <XStack flex={1} justifyContent='space-between'>
                 <Text>{category.name}</Text>
-                <IconX size={20} color="red" />
+                <IconX size={20} color="red" onPress={() => handleDeleteCategory(category.name)} />
               </XStack>
             </ListItem>
           </YGroup.Item>
         ))}
       </YGroup>
-
-      <YStack gap="$2">
-        <Input placeholder='New Category...' />
-        <Button>Add Category</Button>
-      </YStack>
     </YStack>
   );
 }
